38f9cfa8f52b4f2b394b48760c2e18d85eaa646d
jckarinen <91632899+jckarinen@users.noreply.github.com>
2025-11-27 18:38:47 -0700

    [content] revise bio

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -14,7 +14,7 @@ function App() {
                     <a href="https://github.com/jckarinen" target="_blank">GitHub</a>
                 </div>
                 <div className="desc">
-                    <p>Recent CS grad focused on becoming a half-decent programmer.</p>
+                    <p>Recent CS grad on a mission to become a half-decent programmer.</p>
                     <p>Most of my paid work experience has been in maintaining a student-written PHP project that somehow turned into a mission-critical application with thousands of daily users and a codebase spanning over a decade.</p>
                     <p>I like web stuff – I've also recently developed an interest in lower-level programming and am currently reinventing wheels from scratch in C.</p>
                     <p>Learning and building every day.</p>
